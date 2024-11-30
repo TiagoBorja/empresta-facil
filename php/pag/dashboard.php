52182bd5c339fc67d5195133443ab69f0f110fe6
TiagoBorja <borjatiago05@gmail.com>
2024-11-30 17:54:58 +0000

    SPA and static book page

---
+++ php/pag/dashboard.php
@@ -0,0 +1,100 @@
+<div class="row">
+
+    <div class="col-md-6 col-lg-4 col-xlg-3">
+        <a href="?pagina=1">
+            <div class="card card-hover">
+                <div class="box bg-info text-center">
+                    <h1 class="font-light text-white">
+                        <i class="mdi mdi-book-multiple"></i>
+                    </h1>
+                    <h6 class="text-white">Livros</h6>
+                </div>
+            </div>
+        </a>
+    </div>
+
+    <div class="col-md-6 col-lg-4 col-xlg-3">
+        <a href="?pagina=">
+            <div class="card card-hover">
+                <div class="box bg-success text-center">
+                    <h1 class="font-light text-white">
+                        <i class="mdi mdi-swap-horizontal"></i>
+                    </h1>
+                    <h6 class="text-white">Empréstimos</h6>
+                </div>
+            </div>
+        </a>
+    </div>
+
+    <div class="col-md-6 col-lg-4 col-xlg-3">
+        <a href="?pagina=">
+            <div class="card card-hover">
+                <div class="box bg-orange text-center">
+                    <h1 class="font-light text-white">
+                        <i class="mdi mdi-account"></i>
+                    </h1>
+                    <h6 class="text-white">Autores</h6>
+                </div>
+            </div>
+        </a>
+    </div>
+
+</div>
+
+<br>
+
+<div class="row">
+
+    <div class="col-md-6 col-lg-3 col-xlg-3">
+        <a href="?pagina=">
+            <div class="card card-hover">
+                <div class="box bg-primary text-center">
+                    <h1 class="font-light text-white">
+                        <i class="mdi mdi-pencil"></i>
+                    </h1>
+                    <h6 class="text-white">Editoras</h6>
+                </div>
+            </div>
+        </a>
+    </div>
+
+    <div class="col-md-6 col-lg-3 col-xlg-3">
+        <a href="?pagina=">
+            <div class="card card-hover">
+                <div class="box bg-warning text-center">
+                    <h1 class="font-light text-white">
+                        <i class="mdi mdi-account-box"></i>
+                    </h1>
+                    <h6 class="text-white">Utilizadores</h6>
+                </div>
+            </div>
+        </a>
+    </div>
+
+    <div class="col-md-6 col-lg-3 col-xlg-3">
+        <a href="?pagina=">
+            <div class="card card-hover">
+                <div class="box bg-dark text-center">
+                    <h1 class="font-light text-white">
+                        <i class="mdi mdi-account-multiple"></i>
+                    </h1>
+                    <h6 class="text-white">Funcionários</h6>
+                </div>
+            </div>
+        </a>
+    </div>
+
+    <div class="col-md-6 col-lg-3 col-xlg-3">
+        <a href="?pagina=">
+            <div class="card card-hover">
+                <div class="box bg-danger text-center">
+                    <h1 class="font-light text-white">
+                        <i class="mdi mdi-paperclip"></i>
+                    </h1>
+                    <h6 class="text-white">Relatórios</h6>
+                </div>
+            </div>
+        </a>
+    </div>
+
+</div>
