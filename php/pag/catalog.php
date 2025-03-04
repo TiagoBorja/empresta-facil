448dd34d49a27348d12959c1bf3b01bfa2c0cd20
TiagoBorja <borjatiago05@gmail.com>
2025-03-04 21:45:20 +0000

    Removed zoom in the img and go to the resource info

--- php/pag/catalog.php
+++ php/pag/catalog.php
@@ -1,9 +1,16 @@
-<div class="row justify-content-center mb-4 mt-4">
-    <div class="col-md-12">
+<style>
+    .el-card-avatar img {
+        transition: none !important;
+        transform: none !important;
+    }
+</style>
+
+<div class="row justify-content-center">
+    <div class="col-md-12 p-4">
         <div class="card shadow-lg">
             <div class="card-body">
                 <h4 class="card-title text-center mb-4">Pesquisar Catálogo</h4>
-                
+
                 <div class="input-group">
                     <div class="input-group-prepend">
                         <button class="btn btn-outline-secondary dropdown-toggle" type="button" id="filterDropdown"
@@ -41,34 +48,18 @@
 </div>
 
 
-
-<div class="row el-element-overlay">
-    <div class="col-lg-3 col-md-6 mb-3">
-        <div class="card shadow">
-            <div class="el-card-item">
-                <div class="el-card-avatar el-overlay-1">
-                    <img src="../assets/images/big/img1.jpg" alt="user">
-                    <div class="el-overlay">
-                        <ul class="list-style-none el-info">
-                            <li class="el-item">
-                                <a class="
-                              btn
-                              default
-                              btn-outline
-                              image-popup-vertical-fit
-                              el-link
-                            " href="?page=view-info"><i class="mdi mdi-magnify-plus"></i></a>
-                            </li>
-                            <li class="el-item">
-                                <a class="btn default btn-outline el-link" href="javascript:void(0);"><i
-                                        class="mdi mdi-heart-outline"></i></a>
-                            </li>
-                        </ul>
+<div class="col-md-12 p-4">
+    <div class="row el-element-overlay">
+        <div class="col-lg-3 col-md-6 mb-3">
+            <div class="card shadow-lg">
+                <div class="el-card-item">
+                    <div class="el-card-avatar el-overlay-1">
+                        <img src="../assets/images/big/img1.jpg" alt="user">
                     </div>
-                </div>
-                <div class="el-card-content">
-                    <h4 class="mb-0">Project title</h4>
-                    <span class="text-muted">subtitle of project</span>
+                    <div class="el-card-content">
+                        <a class="h4 d-block mb-0" href="?page=view-info">Abelha Zarelha</a>
+                        <span class="text-muted">Raquel Patriarca</span>
+                    </div>
                 </div>
             </div>
         </div>
