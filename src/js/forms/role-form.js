9ab8df71cf64645138f86eb98be2116d82401b9b
TiagoBorja <borjatiago05@gmail.com>
2025-05-12 16:41:27 +0100

    fix: fix incorrects paths

--- src/js/forms/role-form.js
+++ src/js/forms/role-form.js
@@ -1,4 +1,4 @@
-import * as utils from './utils.js';
+import * as utils from '../utils/utils.js';
 
 document.addEventListener('DOMContentLoaded', async function () {
     const urlParams = new URLSearchParams(window.location.search);
